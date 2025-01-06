6d508f81e38d5bbb97f7b68c5925397ff506c9c6
brunorwanda4 <brunorwanda4@gmail.com>
2025-01-06 16:45:25 +0200

    Refactor twoSum function for improved readability and performance

--- two-sum/main.ts
+++ two-sum/main.ts
@@ -1,14 +1,11 @@
 function twoSum(nums: number[], target: number): number[] {
   const sum_numbers: number[] = [];
   for (let i = 0; i < nums.length; i++) {
-    const num1 = nums[i];
     for (let j = 0; j < nums.length; j++) {
-      const num2 = nums[j];
       if (i === j) {
-        continue
+        continue;
       }
-      const sum = num1 + num2;
-      if (sum === target) {
+      if (nums[i] + nums[j] === target) {
         sum_numbers.push(i);
         sum_numbers.push(j);
         return sum_numbers;
@@ -18,7 +15,7 @@ function twoSum(nums: number[], target: number): number[] {
   return sum_numbers;
 }
 
-const nums = [3,2,4];
+const nums = [3, 2, 4];
 const target = 6;
 
 console.log(twoSum(nums, target));
